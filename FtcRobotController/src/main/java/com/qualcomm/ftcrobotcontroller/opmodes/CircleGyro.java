14ef7b270c9b931f4b3409bdbb2c0ec3f75c6d01
etiennelunetta <etiennelunetta@lhsla.org>
2015-11-21 18:28:06 -0800

    GyroData

---
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/CircleGyro.java
@@ -0,0 +1,168 @@
+/* Copyright (c) 2015 Qualcomm Technologies Inc
+
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without modification,
+are permitted (subject to the limitations in the disclaimer below) provided that
+the following conditions are met:
+
+Redistributions of source code must retain the above copyright notice, this list
+of conditions and the following disclaimer.
+
+Redistributions in binary form must reproduce the above copyright notice, this
+list of conditions and the following disclaimer in the documentation and/or
+other materials provided with the distribution.
+
+Neither the name of Qualcomm Technologies Inc nor the names of its contributors
+may be used to endorse or promote products derived from this software without
+specific prior written permission.
+
+NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE GRANTED BY THIS
+LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+"AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
+FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE. */
+
+package com.qualcomm.ftcrobotcontroller.opmodes;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.GyroSensor;
+import com.qualcomm.robotcore.hardware.DcMotor;
+
+/*
+ *
+ * This is an example LinearOpMode that shows how to use
+ * the Modern Robotics Gyro.
+ *
+ * The op mode assumes that the gyro sensor
+ * is configured with a name of "gyro".
+ *
+ *
+ *
+ */
+
+public class CircleGyro extends LinearOpMode {
+
+    DcMotor right;
+    DcMotor left;
+    double speed = 0.00;
+    double targetHeading = 0.0;
+    double gain = 0.007;
+    double steeringError;
+    double leftPower;
+    double rightPower;
+    int currentHeading = 0;
+    double steeringAdjustment = 0;
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+        right = hardwareMap.dcMotor.get("right");
+        left = hardwareMap.dcMotor.get("left");
+        right.setDirection(DcMotor.Direction.REVERSE);
+        GyroSensor sensorGyro;
+
+
+        // write some device information (connection info, name and type)
+        // to the log file.
+        hardwareMap.logDevices();
+
+        // get a reference to our GyroSensor object.
+        sensorGyro = hardwareMap.gyroSensor.get("gyro");
+
+        // calibrate the gyro.
+        sensorGyro.calibrate();
+
+        // wait for the start button to be pressed.
+        waitForStart();
+
+        // make sure the gyro is calibrated.
+        while (sensorGyro.isCalibrating()) {
+            Thread.sleep(50);
+        }
+
+        while (opModeIsActive()) {
+
+            // get the heading info.
+            // the Modern Robotics' gyro sensor keeps
+            // track of the current heading for the Z axis only.
+            currentHeading = sensorGyro.getHeading();
+            if (currentHeading > 180){
+                currentHeading -= 360;
+            }
+
+            steeringError = currentHeading - targetHeading;
+
+            steeringAdjustment = steeringError * gain;
+
+            rightPower = (speed - steeringAdjustment);
+            leftPower = (speed + steeringAdjustment);
+
+            if (rightPower < 0.0) {
+                rightPower = 0.0;
+            }
+            if (leftPower < 0.0) {
+                leftPower = 0.0;
+            }
+            if (rightPower > 1.0) {
+                rightPower = 1.0;
+            }
+            if (leftPower > 1.0) {
+                leftPower = 1.0;
+            }
+
+
+
+            right.setPower(rightPower);
+            left.setPower(leftPower);
+
+
+           /*     if (rightPower > 1.0 || rightPower < -1.0) {
+                    right.setPower(1.0);
+                } else {
+                    right.setPower(rightPower);
+                }
+                if (leftPower > 1.0 || rightPower < -1.0) {
+                    left.setPower(1.0);
+                } else {
+                    left.setPower(leftPower);
+                }
+
+               */ if (gamepad1.a && gamepad1.b) {
+                // reset heading.
+                sensorGyro.resetZAxisIntegrator();
+                Thread.sleep(50);
+            }
+            //  }
+
+
+
+
+
+
+    /* if (heading < 45){
+
+        right.setPower(0.5);
+        left.setPower(-0.5);
+      } else {
+
+        right.setPower(0);
+        left.setPower(0);
+
+      } */
+
+            // if the A and B buttons are pressed, reset Z heading.
+
+            telemetry.addData("1. h", String.format("%03d", currentHeading));
+
+            Thread.sleep(10);
+
+        }
+    }
+}
