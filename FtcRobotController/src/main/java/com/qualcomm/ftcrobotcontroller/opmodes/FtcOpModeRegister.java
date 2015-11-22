a5d4e2dd9da43b7a6b9ea14fd350d12243945f0d
etiennelunetta <etiennelunetta@lhsla.org>
2015-11-21 18:24:58 -0800

    GyroData

--- FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/FtcOpModeRegister.java
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/FtcOpModeRegister.java
@@ -65,6 +65,7 @@ public class FtcOpModeRegister implements OpModeRegister {
     manager.register ("C4TankDrive", C4TankDrive.class);
     manager.register("C4DisplayBot", C4DisplayBot.class);
     manager.register("GyroData", GyroData.class);
+    manager.register("CircleGyro", CircleGyro.class);
 
 
 
